b5a58583f893422cc2fdfeda6e2891b309ecee94
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-07-04 01:26:42 +0300

    PeerId.js

---
+++ src/components/PeerId.js
@@ -0,0 +1,30 @@
+import { memo } from 'react';
+import PropTypes from 'prop-types';
+import { FlexRow, FlexOne, Text } from 'ui/es';
+import { useTheme } from '~/hooks';
+
+const PeerId = ({ peerId }) => {
+  const { weight } = useTheme();
+
+  return (
+    <FlexRow paddingLeft={10} paddingRight={10} fontSize="80%">
+      <Text marginBottom={5} fontWeight={weight.bold} flexBasis={60}>
+        Peer Id
+      </Text>
+      {peerId && (
+        <FlexOne is={Text} workBreak="break-all">
+          {peerId}
+        </FlexOne>
+      )}
+    </FlexRow>
+  );
+};
+
+PeerId.propTypes = {
+  peerId: PropTypes.string,
+};
+PeerId.defaultProps = {
+  peerId: '',
+};
+
+export default memo(PeerId);

81808f643a666b72da3fa0d26875165e0f8ec66e
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-07-04 01:08:34 +0300

    toLocaleDate.js

---
+++ src/utils/toLocaleDate.js
@@ -0,0 +1,21 @@
+/**
+ * Convert to date to local string
+ * @param {Date|string} date
+ * @param {Object?} options
+ * @returns {String}
+ */
+
+function toLocaleDate(
+  date,
+  options = {
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  }
+) {
+  return new Date(date).toLocaleDateString(undefined, options);
+}
+
+export default toLocaleDate;

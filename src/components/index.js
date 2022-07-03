4a722d2f417cb940aac962a8d91feb014d2417fd
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-07-04 01:34:55 +0300

    index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Menu } from './Menu';
+export { default as Diagram } from './Diagram';
+export { default as Sheet } from './Sheet';
+export { default as NotiBar } from './NotiBar';
+export { default as PeerId } from './PeerId';
+export { default as Chat } from './Chat';

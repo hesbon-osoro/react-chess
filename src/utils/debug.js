824c6a5057c263a4894233203b4e1f443a017698
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-07-04 01:08:24 +0300

    debug.js

---
+++ src/utils/debug.js
@@ -0,0 +1,34 @@
+import { curry } from 'ramda';
+import { IS_DEV } from '~/presets';
+
+/**
+ * Debug only
+ * @param {Object?} [options={}]
+ * @return {Function} curry
+ */
+
+function createDebug(options = {}) {
+  options = {
+    mode: 'log',
+    ...options,
+  };
+  /**
+   * @param {String} label
+   * @param {...*}
+   * @return {*}
+   */
+
+  return (label, ...v) => {
+    if (IS_DEV) {
+      console[options.mode](label, ...v);
+    }
+    return v;
+  };
+}
+
+const debug = curry(createDebug());
+
+debug.err = curry(createDebug({ mode: 'error' }));
+debug.inline = createDebug();
+
+export default debug;

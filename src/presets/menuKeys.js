6276f0adbb0d55f4524edad36dbc93c9a93fe812
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-07-04 01:12:47 +0300

    menuKeys.js

---
+++ src/presets/menuKeys.js
@@ -0,0 +1,7 @@
+export const ONE_VS_ONE = '1v1';
+export const ONE_VS_CPU = '1vscpu';
+export const SAVE = 'save';
+export const IMPORT = 'import';
+export const EXPORT = 'export';
+export const ONLINE = 'online';
+export const FLIP = 'flip';

dd5f7b549dc4e8dd598fd4a6fc51354940f7a078
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-07-11 11:34:17 +0300

    NotationBody.js

---
+++ src/components/sheet/NotationBody.js
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+import Box from 'ui-box';
+import { FlexRow } from 'ui/es';
+import { useTheme } from '~/hooks';
+
+const NotationBody = ({ data, children }) => {
+  const { border } = useTheme();
+
+  return (
+    <Box>
+      {data.map((item, index) => {
+        return (
+          <FlexRow
+            key={index}
+            justifyContent="space-between"
+            borderBottom={border}
+          >
+            {children(item)}
+          </FlexRow>
+        );
+      })}
+    </Box>
+  );
+};
+
+NotationBody.propTypes = {
+  children: PropTypes.func.isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      white: PropTypes.shape({
+        from: PropTypes.arrayOf(PropTypes.string),
+        to: PropTypes.arrayOf(PropTypes.string),
+        check: PropTypes.shape({
+          attackerCode: PropTypes.string,
+          defenders: PropTypes.arrayOf(PropTypes.string),
+          defendTiles: PropTypes.arrayOf(PropTypes.string),
+          dodgeableTiles: PropTypes.arrayOf(PropTypes.string),
+        }),
+      }),
+      black: PropTypes.shape({
+        from: PropTypes.arrayOf(PropTypes.string),
+        to: PropTypes.arrayOf(PropTypes.string),
+        check: PropTypes.shape({
+          attackerCode: PropTypes.string,
+          defenders: PropTypes.arrayOf(PropTypes.string),
+          defendTiles: PropTypes.arrayOf(PropTypes.string),
+          dodgeableTiles: PropTypes.arrayOf(PropTypes.string),
+        }),
+      }),
+    })
+  ),
+};
+
+NotationBody.defaultProps = {
+  data: null,
+};
+
+export default NotationBody;

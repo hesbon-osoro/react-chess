c3df757a9ee3620146a985a6be71785ad7f6b492
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-07-12 14:24:29 +0300

    File.js

---
+++ src/components/diagram/File.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import { reverse, map, compose, T, cond, always, identity } from 'ramda';
+import { File as Files, detectDarkTile } from 'chess/es';
+import Tile from './Tile';
+import { useDiagram } from '~/hooks';
+
+const File = ({ rankName }) => {
+  const { getPKey, flip } = useDiagram();
+
+  return compose(
+    map((fileName) => {
+      const isDark = detectDarkTile(fileName, rankName);
+      const tileName = `${fileName}${rankName}`;
+      const pKey = getPKey(tileName);
+
+      return (
+        <Tile key={tileName} isDark={isDark} tileName={tileName} pKey={pKey} />
+      );
+    }),
+    cond([
+      [always(flip), reverse],
+      [T, identity],
+    ])
+  )(Files);
+};
+
+File.propTypes = {
+  rankName: PropTypes.oneOf([PropTypes.string, PropTypes.number]).isRequired,
+};
+
+export default File;

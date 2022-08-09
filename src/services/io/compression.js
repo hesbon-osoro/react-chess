b8498f460f2139160d37fe3d5dd786ca14a2f5ef
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-08-09 12:41:03 +0300

    compression.js

---
+++ src/services/io/compression.js
@@ -0,0 +1,25 @@
+import zlib from 'zlib';
+import { debug } from '~/utils';
+
+export function compress(data) {
+  let compressedStr;
+  try {
+    const str = JSON.stringify(data);
+    compressedStr = zlib.deflateSync(str).toString('base64');
+  } catch (err) {
+    debug.err('zlib - compress issue: ', err);
+  }
+  return compressedStr;
+}
+
+export function decompress(data) {
+  let decompressedStr;
+  try {
+    const buf = Buffer.from(data, 'base64');
+
+    decompressedStr = zlib.inflateSync(buf).toString();
+  } catch (err) {
+    debug.err('zlib - decompress issue: ', err);
+  }
+  return decompressedStr;
+}

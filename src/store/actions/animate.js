c8aa53c46ab30671ca781abc8386c65d242befc9
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-08-10 13:17:45 +0300

    animate.js

---
+++ src/store/actions/animate.js
@@ -0,0 +1,31 @@
+import { compose, last, apply, props, prop } from 'ramda';
+import { Side, getAnimationAxis, Opponent } from 'chess/es';
+import { MEASURE_AXIS } from '../actionTypes';
+
+/**
+ * Measure axis for animation
+ * @param {Array} sheetData
+ * @return {Boolean}
+ */
+
+export function measureAxis(sheetData) {
+  return (dispatch, getState) => {
+    const {
+      general: { flip },
+      network: { side, connected },
+      ingame: {
+        present: { turn },
+      },
+    } = getState();
+
+    dispatch({
+      type: MEASURE_AXIS,
+      payload: compose(
+        apply(getAnimationAxis(flip || (connected && side === Side.black))),
+        props(['from', 'to']),
+        prop(Opponent[turn]),
+        last
+      )(sheetData),
+    });
+  };
+}
